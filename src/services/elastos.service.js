9f1e402737585c72a68c9afcb0206049d9f3cea2
Kiran Pachhai <kiran.pachhai@gmail.com>
2022-01-10 16:40:48 -0500

    Upped the did js sdk version to 2.2.3

--- src/services/elastos.service.js
+++ src/services/elastos.service.js
@@ -5,105 +5,106 @@ import {
   DIDBackend,
   DIDDocument,
   DefaultDIDAdapter,
-} from "@elastosfoundation/did-js-sdk";
+} from '@elastosfoundation/did-js-sdk'
 
 let adapter = {
   createIdTransaction: async (payload, memo) => {
-    let didRequest = JSON.parse(payload);
-    let did = didRequest.proof.verificationMethod;
-    did = did.substring(0, did.indexOf("#")).replace("did:elastos:", "");
+    let didRequest = JSON.parse(payload)
+    let did = didRequest.proof.verificationMethod
+    did = did.substring(0, did.indexOf('#')).replace('did:elastos:', '')
 
-    response[did] = didRequest;
+    response[did] = didRequest
   },
-};
+}
 
-let response = {};
+let response = {}
 
 const Elastos = {
   storepass: process.env.REACT_APP_DID_STORE_PASSWORD,
   GenerateMnemonics: async () => {
-    return new Mnemonic().generate();
+    return new Mnemonic().generate()
   },
   GetIdentity: async (network, mnemonic) => {
-    DIDBackend.initialize(new DefaultDIDAdapter(network));
+    DIDBackend.initialize(new DefaultDIDAdapter(network))
 
-    let store = await DIDStore.open(process.env.REACT_APP_DID_STORE_PATH);
+    let store = await DIDStore.open(process.env.REACT_APP_DID_STORE_PATH)
+    store.synchronize()
     return RootIdentity.createFromMnemonic(
       mnemonic,
-      "",
+      '',
       store,
       Elastos.storepass,
       true
-    );
+    )
   },
   GetDIDDocument: async (network, mnemonic) => {
-    const _network = network === "testnet" ? "testnet" : "mainnet";
-    let identity = await Elastos.GetIdentity(_network, mnemonic);
-    return await identity.newDid(Elastos.storepass, 0, true);
+    const _network = network === 'testnet' ? 'testnet' : 'mainnet'
+    let identity = await Elastos.GetIdentity(_network, mnemonic)
+    return await identity.newDid(Elastos.storepass, 0, true)
   },
   PublishDocument: async (network, mnemonic, profile) => {
-    response = {};
-    const _network = network === "testnet" ? "testnet" : "mainnet";
-    let document = await Elastos.GetDIDDocument(_network, mnemonic);
-    let docBuilder = DIDDocument.Builder.newFromDocument(document);
-    let did = profile.did.replace("did:elastos:", "");
-    docBuilder.edit();
-    docBuilder.setDefaultExpires();
+    response = {}
+    const _network = network === 'testnet' ? 'testnet' : 'mainnet'
+    let document = await Elastos.GetDIDDocument(_network, mnemonic)
+    let docBuilder = DIDDocument.Builder.newFromDocument(document)
+    let did = profile.did.replace('did:elastos:', '')
+    docBuilder.edit()
+    docBuilder.setDefaultExpires()
 
-    if (profile.name && profile.name !== "") {
+    if (profile.name && profile.name !== '') {
       docBuilder = await docBuilder.createAndAddCredential(
         Elastos.storepass,
-        "#name",
+        '#name',
         { name: profile.name },
-        ["SelfProclaimedCredential", "BasicProfileCredential"]
-      );
+        ['SelfProclaimedCredential', 'BasicProfileCredential']
+      )
     }
 
-    if (profile.email && profile.email !== "") {
+    if (profile.email && profile.email !== '') {
       docBuilder = await docBuilder.createAndAddCredential(
         Elastos.storepass,
-        "#email",
+        '#email',
         { email: profile.email },
         [
-          "SelfProclaimedCredential",
-          "BasicProfileCredential",
-          "EmailCredential",
+          'SelfProclaimedCredential',
+          'BasicProfileCredential',
+          'EmailCredential',
         ]
-      );
+      )
     }
 
-    let doc = await docBuilder.seal(Elastos.storepass);
+    let doc = await docBuilder.seal(Elastos.storepass)
     await doc.publish(
       Elastos.storepass,
       doc.getDefaultPublicKeyId(),
       true,
       adapter
-    );
+    )
 
-    let url = `${process.env.REACT_APP_ASSIST_URL}/v1/eidSidechain/publish/didTx`;
+    let url = `${process.env.REACT_APP_ASSIST_URL}/v1/eidSidechain/publish/didTx`
     let data = {
       network: _network,
       didRequest: response[did],
-      memo: "Published from GetDIDs.com",
-    };
+      memo: 'Published from GetDIDs.com',
+    }
 
     let postResponse = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${process.env.REACT_APP_ASSIST_AUTH_TOKEN}`,
       },
       body: JSON.stringify(data),
-    });
+    })
 
-    let json = await postResponse.json();
-    console.log("response: ", json);
+    let json = await postResponse.json()
+    console.log('response: ', json)
 
     return {
       confirmation_id: json.data.didTx.confirmationId,
-      status: "Pending",
-    };
+      status: 'Pending',
+    }
   },
-};
+}
 
-export default Elastos;
+export default Elastos
